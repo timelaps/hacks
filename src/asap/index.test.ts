f488f8f629d2c87877c27fdb84a8df2445a94eff
maikelmclauflin <michael.j.mclaughlin20@gmail.com>
2020-11-15 12:45:03 -0600

    moved coverage to same folder and completed coverage

--- src/asap/index.test.ts
+++ src/asap/index.test.ts
@@ -1,14 +1,34 @@
-import { asap } from './index'
+function importAsap(situations: string[]) {
+  situations.forEach((key) => {
+    switch (key) {
+      case 'setTimeout':
+        global.setTimeout = null as unknown as typeof setTimeout
+      case 'setImmediate':
+        global.setImmediate = null as unknown as typeof setImmediate
+    }
+  })
+  return require('./index')
+}
 
 describe('asap', () => {
-  test('runs async', (done) => {
-    expect.assertions(2)
-    let count = 0
-    asap(() => {
-      expect(count).toBe(1)
-      done()
+  beforeEach(() => {
+    jest.resetModules()
+  });
+  [
+    [],
+    ['setImmediate'],
+    ['setImmediate', 'setTimeout']
+  ].forEach((situation) => {
+    test('runs async', async (done) => {
+      expect.assertions(2)
+      let count = 0
+      const { asap } = await importAsap(situation)
+      asap(() => {
+        expect(count).toBe(1)
+        done()
+      })
+      expect(count).toBe(0)
+      count += 1
     })
-    expect(count).toBe(0)
-    count += 1
   })
 })
